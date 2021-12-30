b640e6456ef362a1a78da03ed7e8f46df53d5843
ValentinFranzen <86569467+ValentinFranzen@users.noreply.github.com>
2021-12-30 11:29:07 +0100

    plan

---
+++ Wir/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Über uns</title>
+</head>
+
+<body>
+    <a href="Team.php">Team</a>
+    <a href="Fahrzeuge.php">Fahrzeuge</a>
+    <a href="Gerätehaus.php">Gerätehaus</a>
+
+</body>
+
+</html>
