a6bc69ae5bb64a4fbe76129e8d44314ad9f0825a
ValentinFranzen <86569467+ValentinFranzen@users.noreply.github.com>
2021-12-30 11:44:22 +0100

    kb

--- Wir/index.php
+++ Wir/index.php
@@ -6,12 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Über uns</title>
+    <link rel="stylesheet" href="../CSS/index.css">
+</head>
 </head>
 
 <body>
+    <div class="button_index" href="Team.php">
+        <img src="img_snow.jpg">
+        <button class="btn">Team</button>
+    </div>
+
+    <!-- 
     <a href="Team.php">Team</a>
     <a href="Fahrzeuge.php">Fahrzeuge</a>
-    <a href="Gerätehaus.php">Gerätehaus</a>
+    <a href="Gerätehaus.php">Gerätehaus</a> -->
 
 </body>
 
